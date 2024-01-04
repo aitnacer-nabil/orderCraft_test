ac447bca43ba96230ff534d37b22efc3cdaa238a
aitnacer-nabil <palancia.gor@gmail.com>
2024-01-04 09:36:30 +0100

    remove all controller

--- src/main/java/com/artjpa/AppConfig/MyWebAppInitializer.java
+++ src/main/java/com/artjpa/AppConfig/MyWebAppInitializer.java
@@ -2,19 +2,21 @@ package com.artjpa.AppConfig;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[] { ApplicationConfig.class };
-    }
+public class MyWebAppInitializer {}
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] { MvcConfig.class };
-    }
+//        extends AbstractAnnotationConfigDispatcherServletInitializer {
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class<?>[] { ApplicationConfig.class };
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class<?>[] { MvcConfig.class };
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[]{"/"};
+//    }
 
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
-}
--- src/main/java/com/artjpa/controller/HomeController.java
+++ src/main/java/com/artjpa/controller/HomeController.java
@@ -6,13 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+
 public class HomeController {
-    @Autowired
-    CustomerServiceImpl customerService;
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message",customerService.getCostumerById(1l).toString());
-        return "home";
-    }
+
 }
